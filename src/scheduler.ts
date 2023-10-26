8e8dd735626dc7d794baa5e519e1545bcdc23052
FE92star <1737765939@qq.com>
2023-10-26 11:34:41 +0800

    feat: add scheduler

---
+++ src/scheduler.ts
@@ -0,0 +1,26 @@
+export class Scheduler {
+  /** 最大并发数 */
+  max = 0
+  /** 当前队列中的任务数 */
+  counts = 0
+  /** 任务队列 */
+  queue: any[] = []
+
+  constructor(max: number) {
+    this.max = max
+  }
+
+  async add(fn: () => Promise<any>) {
+    if (this.counts >= this.max) {
+      await new Promise((resolve) => this.queue.push(resolve))
+    }
+
+    this.counts++
+
+    const res = await fn()
+    this.counts--
+    this.queue.length && this.queue.shift()()
+
+    return res
+  }
+}
