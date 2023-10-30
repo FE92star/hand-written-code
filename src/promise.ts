638b84444f0752b54fece9f78feb93506c748f27
FE92star <1737765939@qq.com>
2023-10-30 10:26:07 +0800

    feat: add promise.all

--- src/promise.ts
+++ src/promise.ts
@@ -60,3 +60,36 @@ export class MyPromise {
     }
   }
 }
+
+// @ts-ignore
+Promise._all = (iterObj) => {
+  // 1. iterObj 必须是一个可迭代对象, 否则, 无法正常进行则抛出错误
+  if (!(typeof iterObj === "object" && iterObj !== null && typeof iterObj[Symbol.iterator] === "function")) {
+    throw new TypeError(`${iterObj} is not iterable`);
+  }
+  iterObj = [...iterObj];
+  /*
+   *  2. 函数返回值为 `<Promise>` 对象, 当参数 `iterObj` 内所有的 `Promise` 成功, 
+   *     该 `<Promise>` 对象成功, 成功数据为所有成功的 `Promise` 结果数组,
+   *     有一个不成功, 则该 `<Promise>` 不成功, 失败数据为失败原因字符串
+   */
+  return new Promise((resolve, reject) => {
+    const len = iterObj.length;
+    let count = 0;
+    if (len === 0) return resolve([]);
+
+    const res = new Array(len);
+    iterObj.forEach(async (item: any, index: number) => {
+      const newItem = Promise.resolve(item);
+      try {
+        const result = await newItem;
+        res[index] = result;
+        if (++count === len) {
+          resolve(res)
+        }
+      } catch (err) {
+        reject(err);
+      }
+    })
+  })
+}
