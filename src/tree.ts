74d9f3b6bf273f794f05d8bc2ebaafaa5d06f465
FE92star <1737765939@qq.com>
2023-12-10 10:11:05 +0800

    feat: add binarySearch function

--- src/tree.ts
+++ src/tree.ts
@@ -57,3 +57,26 @@ export function transformList2Tree(arr: ListNode[]) {
   }
   return list
 }
+
+/**
+ * 二分查找算法
+ * 时间复杂度O(lgN)
+*/
+export function binarySearch(list: number[], target: number) {
+  let left = 0
+  let right = list.length - 1
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2)
+
+    if (list[middle] > target) {
+      right = middle - 1
+    } else if (list[middle] < target) {
+      left = middle + 1
+    } else {
+      return middle
+    }
+  }
+
+  return -1
+}
